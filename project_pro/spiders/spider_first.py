5b727184fc08db0918be0020a410fe46c7f66182
bodyncp <1023128388@qq.com>
2019-01-31 07:43:53 +0800

    分之3次提交

--- project_pro/spiders/spider_first.py
+++ project_pro/spiders/spider_first.py
@@ -11,15 +11,16 @@ from items import ProjectProItem
 
 class SpiderFirstSpider(scrapy.Spider):
     name = 'spider_first'
+    new_url = []
     # allowed_domains = ['http://www.ccgp-sichuan.gov.cn']
     header_url_list = url_data_count['urls']
     start_urls = header_url_list
     header = settings.USER_AGENT
 
     def parse(self, response):
+        current_request_url = response.url
         # 每页文章中的下级文章内容链接
         header_data = header_parse(response)
-
         # 验证页面是否为空
         if header_data:
             # 获取首页的页码数
@@ -29,32 +30,32 @@ class SpiderFirstSpider(scrapy.Spider):
             for page in range(pageup):
                 # 每一个地市的所有页的URL
                 page += 1
-                for page_url in self.start_urls:
-                    page_header_url = "%s&%s=%s" % (page_url, 'curPage', page)
-                    # 拼接每页中标公告的URL
-                    yield scrapy.Request(page_header_url, callback=self.parse_many_page, headers={"User-Agent": self.header})
+                # for page_url in self.start_urls:
+                #     page_header_url = "%s&%s=%s" % (page_url, 'curPage', page)
+                # 拼接每页中标公告的URL
+                current_request_url_new = "%s&%s=%s" % (current_request_url, 'curPage', page)
+                yield scrapy.Request(current_request_url_new, callback=self.parse_many_page, headers={"User-Agent": self.header})
 
     def parse_many_page(self, response):
+        print(response.url)
         header_data = header_parse(response)
         article_url = desc_url + header_data
         yield scrapy.Request(article_url, callback=self.parse_page, headers={"User-Agent": self.header})
 
     def parse_page(self, response):
+        print('2',response.url)
         item = ProjectProItem()
         article_data = response.text
 
         if re.search(key, article_data):
-            public_func(re, key, time_key, article_data, grep_title, time_key_new)
+            public_func(re, key, time_key, article_data, grep_title, time_key_new, item)
 
         elif re.search(key2, article_data):
-            public_func(re, key2, time_key, article_data, grep_title, time_key_new)
+            public_func(re, key2, time_key, article_data, grep_title, time_key_new, item)
 
         elif re.search(key3, article_data):
-            public_func(re, key3, time_key, article_data, grep_title, time_key_new)
+            public_func(re, key3, time_key, article_data, grep_title, time_key_new, item)
 
         elif re.search(key4, article_data):
-            public_func(re, key4, time_key, article_data, grep_title, time_key_new)
-        else:
-            print('没有匹配到')
-
+            public_func(re, key4, time_key, article_data, grep_title, time_key_new, item)
         return item
--- project_pro/spiders/utils.py
+++ project_pro/spiders/utils.py
@@ -10,11 +10,11 @@ import os
 import json
 
 base_dir = os.path.dirname(os.path.dirname(__file__))
+all_zb_data_dict = []
+all_cj_data_dict = []
 
 
-def public_func(re, key, time_key, article_data, grep_title, time_key_new):
-    all_zb_data_dict = []
-    all_cj_data_dict = []
+def public_func(re, key, time_key, article_data, grep_title, time_key_new, item):
     data_title = re.findall(grep_title, article_data)[0]
     data_content = re.findall(key, article_data)[0]
     try:
@@ -23,6 +23,9 @@ def public_func(re, key, time_key, article_data, grep_title, time_key_new):
         date_time = re.findall(time_key_new, article_data)[0]
 
     print(data_title, data_content, date_time)
+    item['title'] = data_title
+    item['content'] = data_content
+    item['time'] = date_time
     all_zb_data_dict.append((data_title, data_content, date_time))
-    with open(os.path.join(base_dir, 'result,', 'result.json'), 'w', encoding='utf-8') as fw:
+    with open(os.path.join(base_dir, 'result', 'result.json'), 'w', encoding='utf-8') as fw:
         fw.write(json.dumps(all_zb_data_dict))
