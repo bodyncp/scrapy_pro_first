253b63f5cda3504bf2c1d0a4f2299a4692336f52
bodyncp <1023128388@qq.com>
2019-01-31 21:59:31 +0800

    四次提交

--- project_pro/result/test.py
+++ project_pro/result/test.py
@@ -4,3 +4,47 @@
 # @Author  : NCP
 # @File    : test.py
 # @Software: PyCharm
+
+import requests
+# import base64
+#
+#
+# def base_code(username, password):
+#     str = '%s:%s' % (username, password)
+#     encodestr = base64.b64encode(str.encode('utf-8'))
+#     return '%s' % encodestr.decode()
+#
+# url = "http://myip.ipip.net/"
+#
+# ip_port = 'xx.xx.xx.xx:xxxx' # 从api中提取出来的代理IP:PORT
+# username = '1023128388@qq.com'
+# password = 'Asd3236119'
+#
+# basic_pwd = base_code(username, password)
+#
+# headers = {
+#     'Proxy-Authorization': 'Basic %s' % (base_code(username, password))
+# }
+#
+# proxy = {
+#     'http' : 'socks5://{}'.format(ip_port),
+#     'https' : 'socks5://{}'.format(ip_port)
+# }
+#
+# r = requests.get(url,proxies=proxy, headers=headers)
+# print(r.text)
+# url = "http://h.wandouip.com/get/ip-list7ce9b5d5d082cb9993522edf15ed290apack=0&num=20&xy=1&type=2&lb=\r\n&mr=1&"
+# r = requests.get(url)
+# print(r.text)
+
+
+# datas = dicts['data']
+#
+# for i in datas:
+#     print(str(i['ip'])+':'+str(i['port']))
+
+a = "1/2,861"
+
+for i in range(int(num)):
+    print(i)
+
--- project_pro/spiders/spider_first.py
+++ project_pro/spiders/spider_first.py
@@ -4,7 +4,7 @@ import re
 import scrapy
 import json
 import settings
-from extra.conf import url_data_count, desc_url, header_parse, key, key2, key3, key4, time_key, time_key_new, grep_title
+from extra.conf import cj_url, desc_url, key, key2, key3, key4, time_key, time_key_new, grep_title
 from spiders.utils import public_func
 from items import ProjectProItem
 
@@ -13,34 +13,35 @@ class SpiderFirstSpider(scrapy.Spider):
     name = 'spider_first'
     new_url = []
     # allowed_domains = ['http://www.ccgp-sichuan.gov.cn']
-    header_url_list = url_data_count['urls']
-    start_urls = header_url_list
-    header = settings.USER_AGENT
+    start_urls = cj_url
 
     def parse(self, response):
         current_request_url = response.url
-        # 每页文章中的下级文章内容链接
-        header_data = header_parse(response)
-        # 验证页面是否为空
-        if header_data:
-            # 获取首页的页码数
-            header_page_num = response.xpath("//div[@class='page-cell']").extract_first()
-            header_page_num = ''.join(re.findall('页次:(.*) </div>', header_page_num))
-            pageup = int(header_page_num.split('/')[-1])
-            for page in range(pageup):
-                # 每一个地市的所有页的URL
-                page += 1
-                # for page_url in self.start_urls:
-                #     page_header_url = "%s&%s=%s" % (page_url, 'curPage', page)
-                # 拼接每页中标公告的URL
-                current_request_url_new = "%s&%s=%s" % (current_request_url, 'curPage', page)
-                yield scrapy.Request(current_request_url_new, callback=self.parse_many_page, headers={"User-Agent": self.header})
+        # 获取首页的页码数
+        header_page_num = response.xpath("//div[@class='page-cell']").extract_first()
+        header_page_num = ''.join(re.findall('页次:(.*) </div>', header_page_num))
+        header_page_num = header_page_num[2::]
+        pageup = ""
+        for num in header_page_num:
+            if num != ",":
+                pageup += num
+        print(pageup)
+        for page in range(int(pageup)):
+            # 每一个地市的所有页的URL
+            page += 1
+            # for page_url in self.start_urls:
+            #     page_header_url = "%s&%s=%s" % (page_url, 'curPage', page)
+            # 拼接每页中标公告的URL
+            current_request_url_new = "%s&%s=%s" % (current_request_url, 'curPage', page)
+            yield scrapy.Request(current_request_url_new, callback=self.parse_many_page)
 
     def parse_many_page(self, response):
         print(response.url)
-        header_data = header_parse(response)
+        header_data = response.xpath("//div[@class='warpper']/div[@class='list-info']/div[@class='info']/ul/li/a/@href").extract()
+        print(header_data)
+        # 此时的header_data 是一个列表
         article_url = desc_url + header_data
-        yield scrapy.Request(article_url, callback=self.parse_page, headers={"User-Agent": self.header})
+        yield scrapy.Request(article_url, callback=self.parse_page)
 
     def parse_page(self, response):
         print('2',response.url)
